28252b251a5ba2fa8a34635f3802ae6ca246796c
songanwei <songanwei@jizhongdiancai.com>
2018-07-17 16:50:41 +0800

    添加订单菜单

--- web/application/common.php
+++ web/application/common.php
@@ -173,6 +173,7 @@ function getFormatPrice($price,$length = 2){
  */
 function getOrderShowStatus(){
     $list = [
+        -1 => '全部',
         1 => '待确认',
         2 => '待付款',
         3 => '待发货',
