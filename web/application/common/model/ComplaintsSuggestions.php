fc8c51ddea659e8e9f3ade957cfb3658ff042d15
songanwei <songanwei@jizhongdiancai.com>
2018-08-27 13:58:18 +0800

    修改后台投诉建议

---
+++ web/application/common/model/ComplaintsSuggestions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alvin
+ * Date: 2018/8/24
+ * Time: 17:41
+ */
+
+namespace app\common\model;
+
+
+use think\Model;
+
+class ComplaintsSuggestions  extends Model{
+
+    protected $table = 'fb_complaints_suggestions';
+
+    protected $prefix = false;
+
+
+
+
+}
