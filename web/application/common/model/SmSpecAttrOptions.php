60a4a3d39c72bb3ba82302428967b5a4009581be
huangjiahui <huangjiahui@jizhongdiancai.com>
2018-08-28 15:25:54 +0800

    模型

---
+++ web/application/common/model/SmSpecAttrOptions.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\common\model;
+
+use think\Model;
+
+class SmSpecAttrOptions extends Model{
+
+	// 设置当前模型对应的完整数据表名称-分类规格明细选项
+    protected $table = 'sm_spec_attr_options';
+    
+	
+}  
