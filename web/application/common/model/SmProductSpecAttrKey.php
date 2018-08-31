2e430bbb38b29036e2d2df03fa95845956a997c4
songanwei <songanwei@jizhongdiancai.com>
2018-08-31 10:10:29 +0800

    新增模型

---
+++ web/application/common/model/SmProductSpecAttrKey.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alvin
+ * Date: 2018/8/31
+ * Time: 10:07
+ */
+
+namespace app\common\model;
+
+
+use think\Model;
+
+class SmProductSpecAttrKey extends Model
+{
+    protected $table = 'sm_product_spec_attr_key';
+
+}
