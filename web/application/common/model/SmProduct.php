b9c75cf555ccb48880a918725440735b0ca657aa
huangjiahui <huangjiahui@jizhongdiancai.com>
2018-09-03 17:30:02 +0800

    后台商品列表相关

--- web/application/common/model/SmProduct.php
+++ web/application/common/model/SmProduct.php
@@ -13,6 +13,9 @@ class SmProduct extends Base{
 
     const AUDIT_PENDING = 2;  //未审核
     const AUDIT_RELEASED = 5; //已发布
+    const AUDIT_SAVED = 1;    //保存的
+    const AUDIT_APPROVED = 3; //已审核通过的
+    const AUDIT_NOTAPPROVED = 4;  //审核不通过的
 
     /**
      * @desc 返回格式化产品图片路径
@@ -23,5 +26,29 @@ class SmProduct extends Base{
         return $img ? config('jzdc_doc_path').'/goods_thumb/'.$img : '';
     }
 
+    /**
+     * @desc 返回详细图
+     * @param $img
+     * @return string
+     */
+    public static function getFormatMultiImg($img){
+        return $img ? config('jzdc_domain').config('jzdc_doc_path').'/goods/'.$img : '';
+    }
 
+
+    /**
+     * [getAuditState 获取商品状态描述]
+     * @param  [type] $id [商品状态]
+     * @return [type]     [description]
+     */
+    public function getAuditState($id){
+        $auditState = [
+            '1' => '草稿',
+            '2' => '待审核',
+            '3' => '审核通过',
+            '4' => '审核不通过',
+            '5' => '发布'
+        ];
+        return isset($auditState[$id])?$auditState[$id]:'';
+    }
 }  
