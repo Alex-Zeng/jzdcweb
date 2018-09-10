b972580b7e154c87a04f214b1f5d8a86832eb6f5
songanwei <songanwei@jizhongdiancai.com>
2018-09-10 15:23:46 +0800

    '添加物料规格列表接口'

--- web/application/api/controller/Material.php
+++ web/application/api/controller/Material.php
@@ -125,6 +125,107 @@ class Material extends Base{
 
 
     /**
+     * @desc PC端列表接口
+     * @param Request $request
+     * @return array|void
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function getWebList(Request $request){
+        $pageSize = $request->post('pageSize',10,'intval');
+        $pageNumber = $request->post('pageNumber',1,'intval');
+        $materialCode = $request->post('materialCode','','trim');
+        $materialSpec = $request->post('materialSpec','','trim');
+        $supplier = $request->post('supplierName','','trim');
+        $start = ($pageNumber - 1)*$pageSize;
+
+        $auth = $this->auth();
+        if($auth){
+            return $auth;
+        }
+
+        //查询物料规格数量
+        $model = new \app\common\model\UserGoodsSpecifications();
+        $where = [
+            'a.user_id' => $this->userId,
+            'c.state' => SmProduct::STATE_FORSALE,
+            'c.audit_state' => SmProduct::AUDIT_RELEASED,
+            'c.is_deleted' =>0,
+            'b.is_deleted' => 0
+        ];
+
+        if($materialCode){
+            $where['a.specifications_no'] = ['like','%'.$materialCode.'%'];
+        }
+        if($materialSpec){
+            $where['a.specifications_name'] = ['like','%'.$materialSpec.'%'];
+        }
+        if($supplier){
+            $where['d.real_name'] = ['like','%'.$supplier.'%'];
+        }
+
+       // $field = ['a.id','a.specifications_no','a.specifications_name','a.product_spec_id','b.sku_code','b.spec_set','b.is_customized','b.spec_img_url','b.min_order_qty','c.title','c.cover_img_url'];
+        $field = ['a.specifications_no','a.specifications_name'];
+        $total = $model->alias('a')
+            ->join(['sm_product_spec' => 'b'],'a.product_spec_id = b.id','left')
+            ->join(['sm_product' => 'c'],'b.product_id=c.id','left')
+            ->join(['jzdc_index_user' => 'd'],'c.supplier_id=d.id','left')
+            ->where($where)->group('a.specifications_no')->count();
+
+        $rows = $model->alias('a')
+            ->join(['sm_product_spec' => 'b'],'a.product_spec_id = b.id','left')
+            ->join(['sm_product' => 'c'],'b.product_id=c.id','left')
+            ->join(['jzdc_index_user' => 'd'],'c.supplier_id=d.id','left')
+            ->where($where)->group('a.specifications_no')->limit($start,$pageSize)->field($field)->select();
+
+        $specAttrValModel = new SmProductSpecAttrVal();
+
+        $list = [];
+        foreach ($rows as $row){
+            //根据物料查询商品
+            $specRows = $model->alias('a')
+                ->join(['sm_product_spec' => 'b'],'a.product_spec_id = b.id','left')
+                ->join(['sm_product' => 'c'],'b.product_id=c.id','left')
+                ->where(['a.user_id'=>$this->userId, 'c.state' => SmProduct::STATE_FORSALE, 'c.audit_state' => SmProduct::AUDIT_RELEASED, 'c.is_deleted' =>0, 'b.is_deleted' => 0])
+                ->field(['a.id','a.specifications_no','a.specifications_name','a.product_spec_id','b.sku_code','b.spec_set','b.is_customized','b.spec_img_url','b.min_order_qty','c.title','c.cover_img_url'])
+                ->select();
+            $specList = [];
+            foreach ($specRows as $specRow){
+                $specInfo = '';
+                if($specRow->is_customized == 1){
+                    $specInfo = '定制';
+                }else{
+                    //
+                    $specSetArr = explode(',',$specRow->spec_set);
+                    $specAttrValRows = $specAttrValModel->where(['id'=>['in',$specSetArr]])->select();
+                    foreach ($specAttrValRows as $specAttrValRow){
+                        $specInfo .= $specAttrValRow->spec_attr_val.',';
+                    }
+                    $specInfo = $specInfo ? substr($specInfo,0,strlen($specInfo)-1) : $specInfo;
+                }
+                $specList[] = [
+                    'materialId' => $specRow->id,
+                    'specId' => $specRow->product_spec_id,
+                    'title' => $specRow->title,
+                    'skuCode' => $specRow->sku_code,
+                    'minOrderQty' => $specRow->min_order_qty,
+                    'specInfo' => $specInfo,
+                    'imgUrl' => $specRow->spec_img_url ? SmProductSpec::getFormatImg($specRow->spec_img_url) : SmProduct::getFormatImg($specRow->cover_img_url)
+                ];
+            }
+
+            $list[] = [
+                'materialCode' => $row->specifications_no,
+                'materialSpec' => $row->specifications_name,
+                "detail" => $specList
+            ];
+        }
+
+        return ['status'=>0,'data'=>['total'=> $total,'list'=>$list],'msg'=>''];
+    }
+
+    /**
      * @desc 修改物料
      * @param Request $request
      * @return array|void
