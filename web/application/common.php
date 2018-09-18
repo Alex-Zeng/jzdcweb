552d52eef373d8b2750a0df7e55dfb5af465d717
songanwei <songanwei@jizhongdiancai.com>
2018-09-18 10:26:31 +0800

    '修复php方法不支持'

--- web/application/common.php
+++ web/application/common.php
@@ -393,3 +393,18 @@ function getSimplePrice($isDiscuss = 0, $price){
 function getBinDecimal($binT){
     return bindec($binT);
 }
+
+/**
+ * @desc php不支持方法则自定义
+ */
+if (!function_exists('getallheaders'))
+{
+    function getallheaders(){
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
